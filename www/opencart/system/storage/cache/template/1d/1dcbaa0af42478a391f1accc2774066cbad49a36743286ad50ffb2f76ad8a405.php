2119c4b8a31a013cad149d20b26d733a2bfe052b
Ivan Korolev <sociopart@hotmail.com>
2022-11-10 17:22:48 +0400

    Add Opencart RU 3.0.3.8

---
+++ www/opencart/system/storage/cache/template/1d/1dcbaa0af42478a391f1accc2774066cbad49a36743286ad50ffb2f76ad8a405.php
@@ -0,0 +1,70 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* common/footer.twig */
+class __TwigTemplate_ce19ee95cb83e8b9a459602e1ae7c5db871b47e8ddc897d5f223d50f18164174 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<footer>
+  <div class=\"container\"><a href=\"http://opencart-russia.ru\" target=\"_blank\">";
+        // line 2
+        echo ($context["text_project"] ?? null);
+        echo "</a>|<a href=\"http://forum.opencart-russia.ru/threads/ustanovka.6044/\" target=\"_blank\">";
+        echo ($context["text_documentation"] ?? null);
+        echo "</a>|<a href=\"http://forum.opencart-russia.ru\" target=\"_blank\">";
+        echo ($context["text_support"] ?? null);
+        echo "</a><br />
+\t</div>
+</footer>
+</body></html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 2,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "common/footer.twig", "");
+    }
+}
